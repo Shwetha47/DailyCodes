51b1ae2e93b543fdc7dc01b7ad5f891989f11fc7
Shwetha47 <rsshwetha1999@gmail.com>
2019-06-28 23:39:39 +0530

    Add files via upload

---
+++ %pImplementation.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    int *n;
+    char c='a';
+    double d=10.1010;
+    a=10;
+    n=&a;
+    printf("%d \n",&a);
+     printf("%d \n",a);
+      printf("%d \n",n);
+       printf("%d \n",*n);
+        printf("%d \n",&n);
+         printf("%p \n",n);
+          printf("%p \n",&a);
+           printf("%p \n",c);
+            printf("%p \n",d);
+
+
+
+
+
+}
